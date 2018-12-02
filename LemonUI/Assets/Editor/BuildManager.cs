f8c17535d533acae51c90f8d323cc6e2ce5d914b
小怪兽 <wff_1994@126.com>
2018-12-02 22:16:57 +0800

    资源文件提交

---
+++ LemonUI/Assets/Editor/BuildManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildRule
+{
+    public string inPut;
+    public string outPut;
+    public List<string> pattens = new List<string>();
+}
+
+public class BuildManager
+{
+    static List<BuildRule> _rules = new List<BuildRule>();
+    public static List<BuildRule> GetBuildRules(bool isReloadRule = false)
+    {
+        if (!isReloadRule && _rules != null && _rules.Count > 0)
+            return _rules;
+
+        _rules.Clear();
+
+        var ruleFile = Application.dataPath + "/../buildrules.txt";
+        var str = File.ReadAllText(ruleFile);
+        var rules = str.Split('\n');
+        foreach (var item in rules)
+        {
+            BuildRule rule = new BuildRule();
+            var param = item.Split('#');
+            if (param.Length < 3)
+                continue;
+
+            rule.inPut = param[0].Trim();
+            rule.outPut = param[2].Trim();
+
+            var pattens = param[1].Split(',');
+            foreach (var patten in pattens)
+            {
+                rule.pattens.Add(patten.Trim());
+            }
+
+            _rules.Add(rule);
+        }
+
+        return _rules;
+    }
+
+    public static string[] GetAllFiles(string path, List<string> pattens)
+    {
+        DirectoryInfo folder = new DirectoryInfo(path);
+        List<string> ret = new List<string>();
+        foreach (var file in folder.GetFiles())
+        {
+            if (pattens.Contains(file.Extension))
+            {
+                ret.Add(file.Name);
+            }
+        }
+
+        return ret.ToArray();
+    }
+}
---
+++ LemonUI/Assets/Editor/BuildWindow.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TestWindow : EditorWindow
+{
+    [MenuItem("Tools/Build AssetBundles")]
+    private static void Open()
+    {
+        TestWindow test = GetWindow<TestWindow>();
+        test.Show();
+    }
+
+    static string outputPath = "";
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("打包规则");
+        GUILayout.Label("输出目录");
+        EditorGUILayout.EndHorizontal();
+
+        var rules = BuildManager.GetBuildRules();
+        foreach (var item in rules)
+        {
+            EditorGUILayout.BeginHorizontal();
+            outputPath = item.outPut;
+            var strPatten = "/";
+            foreach (var patten in item.pattens)
+            {
+                strPatten += "*" + patten + ",";
+            }
+            GUILayout.TextField(item.inPut + strPatten);
+
+            GUILayout.TextField(item.outPut);
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        GUILayout.Label("");
+
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Mark"))
+        {
+            Mark();
+        }
+
+        if (GUILayout.Button("ReloadRole"))
+        {
+            BuildManager.GetBuildRules(true);
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Build"))
+        {
+            Build();
+        }
+
+        EditorGUILayout.EndVertical();
+    }
+
+    private static void Mark()
+    {
+        var rules = BuildManager.GetBuildRules();
+        foreach (var rule in rules)
+        {
+            var path = Application.dataPath + "/Resources/" + rule.inPut;
+            var files = BuildManager.GetAllFiles(path, rule.pattens);
+
+            foreach (var file in files)
+            {
+                var filePath = "Assets/Resources/" + rule.inPut + "/" + file;
+                var importer = AssetImporter.GetAtPath(filePath);
+                if (importer != null)
+                {
+                    var fileName = file.Split('.')[0];
+                    var tag = fileName.Split('_')[0];
+                    importer.assetBundleName = tag.ToLower();
+                }
+            }
+
+            Debug.Log("[BuildManager] Mark Asstebundle success!");
+        }
+    }
+
+    private static void Build()
+    {
+        var dataPath = Application.dataPath;
+        BuildPipeline.BuildAssetBundles(dataPath + "/" + outputPath,
+          BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+
+        Debug.Log("[BuildManager] Build Asstebundle success!");
+    }
+}
